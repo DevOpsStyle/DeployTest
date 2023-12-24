24e5023d3cdc97bff0dc6b617596afe3a7735687
tommasosacco <tommasosacco@microsoft.com>
2023-12-24 21:06:16 +0100

    test

--- Script.ps1
+++ Script.ps1
@@ -1,1 +1,25 @@
-hostname
+param(
+    [string]$username,
+    [string]$password
+)
+
+# Conversione della password in un oggetto SecureString
+$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+
+# Creazione dell'oggetto credenziale
+$cred = New-Object System.Management.Automation.PSCredential($username, $securePassword)
+
+# Definizione dell'indirizzo IP del computer remoto
+$computerName1 = "192.168.1.12"
+$computerName2 = "192.168.1.13"
+
+# Comando da eseguire sul computer remoto
+$scriptBlock = {
+    # Inserisci qui il comando da eseguire
+    # Esempio: Get-Process
+    Get-Process
+}
+
+# Esecuzione del comando sul computer remoto
+Invoke-Command -ComputerName $computerName1 -ScriptBlock $scriptBlock -Credential $cred
+Invoke-Command -ComputerName $computerName2 -ScriptBlock $scriptBlock -Credential $cred
