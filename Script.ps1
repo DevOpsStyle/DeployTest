c9ce555ba060cefb71eb7f0746a35da2ec5eabe4
tommasosacco <tommasosacco@microsoft.com>
2023-12-25 02:43:14 +0100

    test

--- Script.ps1
+++ Script.ps1
@@ -19,21 +19,15 @@ $scriptBlock = {
     # Esempio: Get-Process
     $ActiveNode = (Get-ClusterGroup | Where-Object { $_.Name -eq "ca-dbd28c5d-2c62-49a3-8751-ae6936c604e5" }).OwnerNode.ToString()
     $hostname = hostname
-    if ($hostname -eq $ActiveNode) {
-      $updates = Get-akshciupdates
-      if ($updates -eq ""){ 
-        Write-Output "$hostname No updates available" 
-      }
-      else { 
-        Write-Output "Updates available" 
-      }
-    else 
-    { 
-      Write-Output "Node Inactive" 
-    }
+    if ($hostname -eq $ActiveNode) {Get-akshciupdates}
+    else { Write-Output "Node Inactive" }
   }
-}
 
 # Esecuzione del comando sul computer remoto
-Invoke-Command -ComputerName $computerName1 -ScriptBlock $scriptBlock -Credential $cred
-Invoke-Command -ComputerName $computerName2 -ScriptBlock $scriptBlock -Credential $cred
+$Host1 = Invoke-Command -ComputerName $computerName1 -ScriptBlock $scriptBlock -Credential $cred
+$Host2 = Invoke-Command -ComputerName $computerName2 -ScriptBlock $scriptBlock -Credential $cred
+
+if ($Host1 -eq "") { $Host1 = "No Updates Available" } else { $Host1 = $Host1 }
+if ($Host2 -eq "") { $Host1 = "No Updates Available" } else { $Host1 = $Host1 }
+Write-output "Host1: $Host1"
+Write-output "Host2: $Host2"
